5c065c9c2e691bc7d77fa1ddd972ff705f333d75
ignacio <ignacio@poli.ufrj.br>
2018-09-19 21:51:46 -0300

    RMSE script

---
+++ Simulink/rmse.m
@@ -0,0 +1,5 @@
+function [RMSE] = rmse(y,yhat)
+(y - yhat);    % Errors
+(y - yhat).^2;   % Squared Error
+mean((y - yhat).^2);   % Mean Squared Error
+RMSE = sqrt(mean((y - yhat).^2));  % Root Mean Squared Error
