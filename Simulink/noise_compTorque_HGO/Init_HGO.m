e71456ffbf216cb9cba682520150ca3c0eeca12a
igricart <ignacio@poli.ufrj.br>
2018-09-20 20:20:09 -0300

    Clean script

--- Simulink/noise_compTorque_HGO/Init_HGO.m
+++ Simulink/noise_compTorque_HGO/Init_HGO.m
@@ -1,22 +1,18 @@
 %% Inicializador do HGO
 
+% Dynamics
 Ar = [zeros(4) eye(4); zeros(4) zeros(4)];
 Br = [zeros(4); eye(4)];
 Cr = [eye(4) zeros(4)];
-
-P = conv(conv(conv([1 5 6],[1 5 6]),[1 5 6]),[1 5 6]);
-
-Lc1 = [P(2:end)]';
-Lc2 = [P(2:end)]';
-Lc3 = [P(2:end)]';
-Lc4 = [P(2:end)]';
-L1 = [Lc1 Lc2 Lc3 Lc4];
-
-Mu = 1*(1e-3);
-
 L_hgo = [2*eye(4);6*eye(4)];
-%L_hgo = L1;
 
-
+% Gain
+Mu = 1*(1e-3);
 Hmu = [eye(4)/Mu zeros(4); zeros(4) eye(4)/Mu^2];
+
+% Initial conditions
+q_hat_init = zeros(4,1);
+% q_hat_init = qInit;
+dq_hat_init = zeros(4,1);
+
 
