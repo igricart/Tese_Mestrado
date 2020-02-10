bb8de8aba4069724486faea8a35c9f4f05c92569
Ignacio Ricart <ignacio@poli.ufrj.br>
2020-02-09 21:09:59 -0300

    add hgo simulation .mat results and Init_HGO changes

--- Simulink/general_sim/Init_HGO.m
+++ Simulink/general_sim/Init_HGO.m
@@ -8,10 +8,12 @@ L_hgo = [2*eye(4);6*eye(4)];
 kpn = eye(4);
 
 % Gain
-Mu_hgo = 19e-4;
+Mu_hgo = 20e-4;
+k_noise = 2e-7;
 Hmu = [eye(4)/Mu_hgo zeros(4); zeros(4) eye(4)/Mu_hgo^2];
 
 % Initial conditions
 q_hat_init = zeros(4,1);
 % q_hat_init = qInit;
+
 dq_hat_init = zeros(4,1);
