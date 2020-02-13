4ba858d0d605738a98e329d28d576c2902bc666d
Ignacio Ricart <ignacio@poli.ufrj.br>
2020-02-12 21:27:27 -0300

    add FFt script to plot the gait spectrum

--- Simulink/aux_folder/FFT_data_step.m
+++ Simulink/aux_folder/FFT_data_step.m
@@ -15,19 +15,20 @@ for k = 1:4
     N = 2^ceil(log2(length(x)) + 3);
     X = abs(fft(x, N));
     freq_fft = (0:(N-1))/N * fs;
-    subplot(4,2,2*k-1);
-    stem(freq_fft, X)
-    title(['FFT from joint ' num2str(k) ' in human gait']);
-    xlim([0 20]);
-    %
-    Y = abs(fft(y, N));
-    subplot(4,2,2*k);
-    stem(freq_fft, Y)
-    title(['FFT from joint ' num2str(k) ' with blackman window']);
-    xlim([0 20]);
-
-    figure(f2);
-    % Plot signals
     subplot(2,2,k);
-    plot(t,x,t,y);
+    grid on;
+    stem(freq_fft, X)
+    title(['Joint ' num2str(k)]);
+    xlim([0 10]);
+    %
+    %Y = abs(fft(y, N));
+    %subplot(4,2,2*k);
+    %stem(freq_fft, Y)
+    %title(['FFT from joint ' num2str(k) ' with blackman window']);
+    %xlim([0 20]);
+
+    %figure(f2);
+    % Plot signals
+    %subplot(2,2,k);
+    %plot(t,x,t,y);
 end
