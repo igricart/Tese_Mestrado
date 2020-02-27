17a5e3d9e663847e4d717228da2d53b76d5d6eae
Ignacio Ricart <ignacio@poli.ufrj.br>
2020-02-27 02:00:57 -0300

    add rmse normalization

--- Simulink/aux_folder/rmse.m
+++ Simulink/aux_folder/rmse.m
@@ -1,5 +1,6 @@
-function [RMSE] = rmse(y,yhat)
-(y - yhat);    % Errors
-(y - yhat).^2;   % Squared Error
-mean((y - yhat).^2);   % Mean Squared Error
-RMSE = sqrt(mean((y - yhat).^2));  % Root Mean Squared Error
+function [RMSE, RMSE_norm] = rmse(y,y_d)
+(y - y_d);    % Errors
+(y - y_d).^2;   % Squared Error
+mean((y - y_d).^2);   % Mean Squared Error
+RMSE = sqrt(mean((y - y_d).^2));  % Root Mean Squared Error
+RMSE_norm = RMSE./max(y_d);
